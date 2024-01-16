3cd7c813ff5eb68c9c73f29a28cb5b2ffeb9bb48
ghanayemomar <o.ghanaiem@student.aaup.edu>
2024-01-16 15:25:56 +0200

    Add project files.

---
+++ EFCORE-08/Program.cs
@@ -0,0 +1,47 @@
+﻿using EF007;
+using System;
+
+namespace EF007
+{
+    class Program
+    {
+        // Step #1: Package Manager Console (PMC)
+        //    Tools -> Nuget Package Manager -> Package Manager Console
+
+        // Step #2: Package Manager Console (PMC) Tool 
+        //    Install-Package Microsoft.EntityFrameworkCore.Tools
+
+        // Step #3: Install Nuget Page on Project Microsoft.EntityFrameworkCore.Design
+        // Microsoft.EntityFrameworkCore.SqlServer
+
+        // Step #4: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+
+        // Step #5: Run Command (Full)
+        //    Scaffold-DbContext '[Connection String]' [Provider]
+
+
+        // ---------------------------------------- Using Terminal ---------------------------------
+
+        // Step #1: Windows Terminal (Command Prompt) 
+
+        // Step #2: Install Ef-Core tool globally
+        //    dotnet tool install --global dotnet-ef    (new)
+        //    dotnet tool update --global dotnet-ef     (to upgrade)
+
+        // Step #3: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+
+        // Step #4: Run Command (Full)
+        //    dotnet ef dbcontext scaffold '[Connection String]' [Provider]
+
+        public static void Main()
+        {
+            //foreach (var item in new TechTalkContext().Speakers)
+            //{
+            //    Console.WriteLine(item.FirstName + " " ,item.LastName); 
+
+            //}
+        }
+
+
+    }
+}
---
+++ EFCORE-08/TechTalkContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCORE_08;
+
+public partial class TechTalkContext : DbContext
+{
+    public TechTalkContext()
+    {
+    }
+
+    public TechTalkContext(DbContextOptions<TechTalkContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source = .; Initial Catalog = TechTalk ; Integrated Security = SSPI; TrustServerCertificate = True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Events__3214EC0752804E47");
+
+            entity.HasOne(d => d.Speaker).WithMany(p => p.Events)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Events__SpeakerI__398D8EEE");
+        });
+
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC078F38C331");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
